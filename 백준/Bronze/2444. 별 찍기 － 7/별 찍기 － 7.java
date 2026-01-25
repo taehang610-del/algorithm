eac021152dba2147e4a35c87a41460cae5cbb88c
taehang610-del <taehang610@gmail.com>
2026-01-26 05:10:14 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 472 ms, Memory: 22484 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        
+        for (int i = 0 ; i <= N - 1 ; i++)
+        {
+            for (int j = 1 ; j < N - i ; j++)
+            {
+                System.out.print(" ");
+            }
+            for (int k = 0 ; k < 2 * i + 1; k++)
+            {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+        for (int i = N - 1 ; i > 0 ; i--)
+        {
+            for (int j = N - i ; j > 0  ; j--)
+            {
+                System.out.print(" ");
+            }
+            for (int k = 2 * i - 1 ; k > 0 ; k--)
+            {
+                System.out.print("*");
+            }
+                System.out.println("");
+        }
+    }
+}
